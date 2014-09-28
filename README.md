fb91df053caa55b0742503e3b4f083d760cf62ce
Max Penet <m@qbits.cc>
2014-09-28 09:20:35 +0200

    initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# commons
+<!-- [![Build Status](https://secure.travis-ci.org/mpenet/commons.png?branch=master)](http://travis-ci.org/mpenet/commons) -->
+
+## Documentation
+
+[A usage guide is available](https://github.com/mpenet/commons/blob/master/docs/guide.md)
+and you can also consult the
+[codox generated documentation](http://mpenet.github.com/commons/#docs).
+
+## Installation
+
+commons is [available on Clojars](https://clojars.org/cc.qbits/commons).
+
+Add this to your dependencies:
+
+```clojure
+[cc.qbits/commons "0.1.0-SNAPSHOT"]
+```
+
+Please check the
+[Changelog](https://github.com/mpenet/commons/blob/master/CHANGELOG.md)
+if you are upgrading.
+
+## License
+
+Copyright © 2014 [Max Penet](http://twitter.com/mpenet)
+
+Distributed under the
+[Eclipse Public License](http://www.eclipse.org/legal/epl-v10.html),
+the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject cc.qbits/commons "0.1.0"
+  :description "Stuff"
+  :url "https://github.com/mpenet/commons"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :dev  {:dependencies []}
+             :test  {:dependencies []}}
+  :codox {:src-dir-uri "https://github.com/mpenet/commons/blob/master"
+          :src-linenum-anchor-prefix "L"
+          :output-dir "doc/codox"}
+  :source-paths ["src/clj"]
+  ;; :java-source-paths ["src/java"]
+  ;; :javac-options ["-source" "1.6" "-target" "1.6" "-g"]
+  :global-vars {*warn-on-reflection* true})
