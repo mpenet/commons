b5e4304286e96d978b7d5f6162275b19d7de4a23
Max Penet <m@qbits.cc>
2015-03-19 09:42:20 +0100

    create map from public class fields

--- src/clj/qbits/commons/enum.clj
+++ src/clj/qbits/commons/enum.clj
@@ -1,14 +1,25 @@
 (ns qbits.commons.enum
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as str]))
+
+(defn format-key
+  [k]
+  (-> k
+      str/lower-case
+      (str/replace "_" "-")
+      keyword))
 
 (defn enum->map
   [enum]
   (reduce
    (fn [m hd]
-     (assoc m (-> (.name ^Enum hd)
-                  (.toLowerCase)
-                  (string/replace "_" "-")
-                  keyword)
-            hd))
+     (assoc m (format-key (.name ^Enum hd)) hd))
    {}
    (java.util.EnumSet/allOf enum)))
+
+(defn fields->map
+  [^Object cls]
+  (reduce
+   (fn [m ^java.lang.reflect.Field f]
+     (assoc m (format-key (.getName f)) (.get f cls)))
+   {}
+   (->> cls bean :declaredFields)))
