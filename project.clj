71a1e81bcdf35958acbf70af022a16ad0d20b575
Max Penet <m@qbits.cc>
2015-07-15 11:53:36 +0200

    add case-enum

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cc.qbits/commons "0.3.0"
+(defproject cc.qbits/commons "0.4.0"
   :description "Stuff"
   :url "https://github.com/mpenet/commons"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
