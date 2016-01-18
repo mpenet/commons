3f806257cffe08bdf64f98e2b96782870f88cbe5
Max Penet <m@qbits.cc>
2016-01-18 09:47:55 +0100

    add compile-if

--- src/clj/qbits/commons/jvm.clj
+++ src/clj/qbits/commons/jvm.clj
@@ -11,3 +11,15 @@
    (reify Thread$UncaughtExceptionHandler
      (uncaughtException [_ thread ex]
        (f thread ex)))))
+
+(defmacro compile-if
+  "Evaluate `exp` and if it returns logical true and doesn't error, expand to
+  `then`.  Else expand to `else`.
+  (compile-if (Class/forName \"java.util.concurrent.ForkJoinTask\")
+    (do-cool-stuff-with-fork-join)
+    (fall-back-to-executor-services))"
+  [exp then & [else]]
+  (if (try (eval exp)
+           (catch Throwable _ false))
+    `(do ~then)
+    `(do ~else)))
