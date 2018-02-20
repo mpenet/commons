073930c7486608c165d53767ce23794a294006a2
Max Penet <m@qbits.cc>
2018-02-20 11:21:36 +0100

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/commons "0.5.0"
+(defproject cc.qbits/commons "0.5.1"
   :description "Stuff"
   :url "https://github.com/mpenet/commons"
   :license {:name "Eclipse Public License"
