cdd52436b7d74a8272921b3fa29898a2e39b8519
Max Penet <max.penet@exoscale.ch>
2020-11-11 14:39:02 +0100

    allow to have ns-ed enum values

--- src/clj/qbits/commons/enum.clj
+++ src/clj/qbits/commons/enum.clj
@@ -5,35 +5,46 @@
    [qbits.commons.string :as s]))
 
 (defn format-key
-  [k]
-  (-> k
-      s/camel->dashed
-      keyword))
+  [k ns]
+  (->> k
+       s/camel->dashed
+       (keyword (some-> ns name))))
+
 
 (defn enum->map
-  [enum]
-  (reduce
-   (fn [m hd]
-     (assoc m (format-key (.name ^Enum hd)) hd))
-   {}
-   (java.util.EnumSet/allOf enum)))
+  ([enum ns]
+   (reduce (fn [m hd]
+             (assoc m (format-key (.name ^Enum hd)
+                                  ns)
+                    hd))
+           {}
+           (java.util.EnumSet/allOf enum)))
+  ([enum]
+   (enum->map enum nil)))
 
-(defn enum->set [enum]
-  (into #{}
-        (map #(format-key (.name ^Enum %)))
-        (java.util.EnumSet/allOf enum)))
+(defn enum->set
+  ([enum] (enum->set enum nil))
+  ([enum ns]
+   (into #{}
+         (map #(format-key (.name ^Enum %)
+                           ns))
+         (java.util.EnumSet/allOf enum))))
 
 (defmacro defspec
-  [k enum]
-  `(spec/def ~k ~(enum->set (eval enum))))
+  [k enum & [ns]]
+  `(spec/def ~k ~(enum->set (eval enum) ns)))
 
 (defn fields->map
-  [^Object cls]
-  (reduce
-   (fn [m ^java.lang.reflect.Field f]
-     (assoc m (format-key (.getName f)) (.get f cls)))
-   {}
-   (->> cls bean :declaredFields)))
+  ([cls] (fields->map cls nil))
+  ([^Object cls ns]
+   (reduce
+    (fn [m ^java.lang.reflect.Field f]
+      (assoc m
+             (format-key (.getName f)
+                         ns)
+             (.get f cls)))
+    {}
+    (->> cls bean :declaredFields))))
 
 (defmacro case-enum
   "Like `case`, but explicitly dispatch on Java enum ordinals."
@@ -50,17 +61,18 @@
 (defmacro enum->fn
   "Like enum->map but >6x faster and throws on invalid enum values
   instead of just returning nil"
-  [enum-symbol]
-  (let [enum (eval enum-symbol)
-        enum-map (enum->map enum)]
-    `(fn [x#]
-       (case
-           x#
-         ~@(mapcat (fn [[k ^Enum e]]
-                     [k (->> (.name e)
-                             (str enum-symbol "/")
-                             symbol)])
-                   enum-map)
-         (throw (ex-info ~(format "Invalid Enum key - possible keys are -> %s"
-                                  (str/join ", " (map key enum-map)))
-                         {:type  ::invalid-enum-value}))))))
+  ([enum-symbol & [ns]]
+   (let [enum (eval enum-symbol)
+         enum-map (enum->map enum ns)]
+     (prn enum-map)
+     `(fn [x#]
+        (case
+            x#
+          ~@(mapcat (fn [[k ^Enum e]]
+                      [k (->> (.name e)
+                              (str enum-symbol "/")
+                              symbol)])
+                    enum-map)
+          (throw (ex-info ~(format "Invalid Enum key - possible keys are -> %s"
+                                   (str/join ", " (map key enum-map)))
+                          {:type  ::invalid-enum-value})))))))
