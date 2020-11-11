e06cbda4705d08d081db8cebc66bb48161b437ef
Max Penet <max.penet@exoscale.ch>
2020-11-11 14:40:32 +0100

    repos

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/mpenet/commons"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/spec.alpha "0.2.176"]]
   :source-paths ["src/clj"]
