80eb03d2115079c5fcadc1cf761744562bd7347b
Max Penet <max.penet@exoscale.ch>
2021-01-05 15:33:40 +0100

    Version 1.0.0-alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/commons "1.0.0-alpha4-SNAPSHOT"
+(defproject cc.qbits/commons "1.0.0-alpha4"
   :description "Stuff"
   :url "https://github.com/mpenet/commons"
   :license {:name "Eclipse Public License"
