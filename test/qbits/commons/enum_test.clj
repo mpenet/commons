b6c9b14e6202bd041e678b7620aa7b91db79926a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2021-01-07 17:20:22 +0000

    test invalid key is reported on the exception message

--- test/qbits/commons/enum_test.clj
+++ test/qbits/commons/enum_test.clj
@@ -1,7 +1,8 @@
 (ns qbits.commons.enum-test
   (:require
    [clojure.test :as t :refer [deftest testing is]]
-   [qbits.commons.enum :as sut])
+   [qbits.commons.enum :as sut]
+   [clojure.string :as str])
   (:import
    [java.math RoundingMode]))
 
@@ -12,14 +13,16 @@
       (is (instance? RoundingMode v))
       (is (= "UP" (str v)))))
 
-  (testing "invalid key is reported in ex-data"
+  (testing "invalid key is reported in ex-data and exception message"
     (let [efn (sut/enum->fn RoundingMode)
           v (try
               (efn :foo)
               (catch Exception err err))
           {exd-t :type
            exd-k :key
-           :as exd} (ex-data v)]
+           :as exd} (ex-data v)
+          ex-msg (when (instance? Exception v) (.getMessage ^Exception v))]
       (is (some? exd))
       (is (= ::sut/invalid-enum-value exd-t))
-      (is (= :foo exd-k)))))
+      (is (= :foo exd-k))
+      (is (str/starts-with? ex-msg "Invalid Enum key - :foo" )))))
