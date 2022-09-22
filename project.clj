65a05f2d593cb4cadbcabe99758bc584825dddb5
Max Penet <mpenet@s-exp.com>
2022-09-22 09:21:17 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/commons "1.0.0-alpha6"
+(defproject cc.qbits/commons "1.0.0-alpha6-SNAPSHOT"
   :description "Stuff"
   :url "https://github.com/mpenet/commons"
   :license {:name "Eclipse Public License"
